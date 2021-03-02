971bc2261de77911db56699e233110f49de30c86
Artem <xthemey@gmail.com>
2021-03-02 18:32:10 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -70,9 +70,54 @@ document.addEventListener('DOMContentLoaded', function () {
 	})
 
 	//mixItUp plugin
-	var mixer = mixitup('#cocktails-list', {
-		classNames: {
-			block: ""
+
+	const cocktailList = document.querySelector("#cocktails-list");
+	if (cocktailList) {
+		mixitup('#cocktails-list', {
+			classNames: {
+				block: ""
+			}
+		});
+	}
+
+
+	const sliders = document.querySelectorAll(".slider-ui");
+
+	sliders.forEach(slider => {
+		let input = slider.querySelector("input[type=range]");
+		let min = input.getAttribute("min");
+		let max = input.getAttribute("max");
+		let valueElem = slider.querySelector(".value");
+
+		slider.querySelector(".min").innerText = min;
+		slider.querySelector(".max").innerText = max;
+		const markups = max / 25
+
+		function setValueElem() {
+			valueElem.innerText = input.value;
+			let percent = (input.value - min) / (max - min) * 100;
+			valueElem.style.left = percent + "%";
 		}
+		setValueElem();
+
+		input.addEventListener("input", setValueElem);
+		input.addEventListener("mousedown", () => {
+			valueElem.classList.add("up");
+		});
+		input.addEventListener("mouseup", () => {
+			valueElem.classList.remove("up");
+		});
 	});
+
+	const slidersMarkUp = document.querySelector(".track")
+
+	
+	// for (let index = 0; index < 12; index++) {
+	// 	let block = document.createElement('div')
+	// 	block.classList.add('markup')
+	// 	block.innerHTML = "<span>25</span>"
+	// 	slidersMarkUp.appendChild(block)
+	// }
+
+
 })
